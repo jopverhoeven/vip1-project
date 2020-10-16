c4023d50bc25895ca3860a77081c9594553f64c9
Jop <jop.verhoeven@gmail.com>
2020-10-16 16:41:20 +0200

    Kermis Eerste Push

---
+++ Assets/Scenes/TestScene/Scripts/RingThrowing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingThrowing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.tag == "RingThrowing") {
+            Material material = other.gameObject.GetComponent<MeshRenderer>().material;
+            material.color = Color.green;
+        }
+    }
+
+    public void OnTriggerExit(Collider other) {
+        if (other.tag == "RingThrowing") {
+            Material material = other.gameObject.GetComponent<MeshRenderer>().material;
+            material.color = Color.white;
+        }
+    }
+
+}
---
+++ Assets/Scenes/TestScene/Scripts/StickToMovingObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickToMovingObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit raycastHit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(ray, out raycastHit, 3f))
+        {
+            if (raycastHit.transform.gameObject.tag == "MovingAlong")
+            {
+                transform.parent = raycastHit.transform;
+            } else {
+                transform.parent = null;
+                transform.rotation = Quaternion.identity;
+            }
+        }
+    }
+}
