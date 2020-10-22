1421d414de84990ffad44b7c200ff599dcacc3e4
Jop <jop.verhoeven@gmail.com>
2020-10-22 18:27:24 +0200

    Added multilayer menu

--- Assets/Scenes/TestScene/Scripts/MenuToggle.cs
+++ Assets/Scenes/TestScene/Scripts/MenuToggle.cs
@@ -5,20 +5,52 @@ using Valve.VR;
 public class MenuToggle : MonoBehaviour
 {
 
-    public GameObject menuObject;
+    public GameObject parent;
+    public GameObject mainMenuObject;
+    public GameObject[] subMenuObjects;
 
     // Start is called before the first frame update
     void Start()
     {
-        menuObject.SetActive(false);
+        foreach (var subMenuObject in subMenuObjects)
+        {
+            subMenuObject.SetActive(false);
+        }
+        mainMenuObject.SetActive(true);
+        parent.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (SteamVR_Actions.default_ToggleMenu.lastStateDown) {
-            menuObject.SetActive(!menuObject.activeSelf);
+            parent.SetActive(!parent.activeSelf);
         }
-       
+    }
+
+    public void GoToSubMenu(int subMenuIndex) 
+    {
+        mainMenuObject.SetActive(false);
+        subMenuObjects[subMenuIndex].SetActive(true);
+    }
+
+    public void GoToSubMenu(GameObject subMenu)
+    {
+        mainMenuObject.SetActive(false);
+        foreach (var subMenuObject in subMenuObjects)
+        {
+            if (subMenuObject == subMenu) {
+                subMenuObject.SetActive(true);
+            }
+        }
+    }
+
+    public void GoBackToMainMenu()
+    {
+        mainMenuObject.SetActive(true);
+        foreach (var subMenuObject in subMenuObjects)
+        {
+            subMenuObject.SetActive(false);
+        }
     }
 }
