7b7ace8ea8325d60a8929b7e373e9edae92b4dda
Jop <jop.verhoeven@gmail.com>
2020-10-28 13:49:50 +0100

    Updated skybox, finished door, added RC Car, updated lighting

---
+++ Assets/Scenes/TestScene/Scripts/EnterDrivableObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class EnterDrivableObject : MonoBehaviour
+{
+    public Transform cameraParent;
+    private Transform attatch;
+    private Transform previousTransform;
+    private bool currentlyDriving;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentlyDriving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!currentlyDriving)
+        {
+            previousTransform = this.transform;
+        }
+        else {
+            this.transform.parent = attatch;
+            this.transform.position = attatch.position;
+            this.transform.rotation = attatch.rotation;
+        }
+    }
+
+    public void EnterDriving(Transform attatch) {
+        this.attatch = attatch;
+        currentlyDriving = true;
+    }
+
+    public void LeaveDriving() {
+        this.transform.parent = cameraParent;
+        this.transform.position = previousTransform.position;
+        this.transform.rotation = previousTransform.rotation;
+
+        currentlyDriving = false;
+    }
+}
